189bc703350abc22d065f8595e19a3f88506bbbb
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2012-04-19 18:00:24 +0000

    Build base as part of all. Refs #3

--- build_all.sh
+++ build_all.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
 set -o errexit
 mydir="`pwd`"
-#. build_base.sh
-cd "$mydir"
 export MY_EPICS_BASE="${mydir}/base"
 export EPICS_BASE_PVT="${mydir}/base"
+#
+cd base
 . ${MY_EPICS_BASE}/startup/Site.profile
+make
+cd "$mydir"
 #
 for i in modules/soft; do
     echo "EPICS_BASE=${MY_EPICS_BASE}" > "$i/configure/EPICS_BASE.${EPICS_HOST_ARCH}"
 done
-#echo 'EPICS_BASE=$(TOP)/../../base' > "modules/soft/configure/EPICS_BASE.${EPICS_HOST_ARCH}"
 #
 cd base_examples
 sh generate_examples.sh
